5288dd70ea7885718a9096905654860df372b900
Vishrut <vishrutkmr7@gmail.com>
2019-08-04 19:03:36 +0530

    Iterative solution donw, recursive faulty

--- dp08032019.py
+++ dp08032019.py
@@ -12,18 +12,47 @@ class ListNode(object):
         node = self
         output = '' 
         while node != None:
-        output += str(node.val)
-        output += " "
-        node = node.next
+            output += str(node.val)
+            output += " "
+            node = node.next
         print(output)
 
     # Iterative Solution
     def reverseIteratively(self, head):
         # Implement this.
+        node = head
+        finalList = []
+        while node != None:
+            finalList.append(node.val)
+            node = node.next
 
-    # Recursive Solution      
+        finalList.reverse()
+        startNode = n = ListNode(finalList[0])
+        for i in range(1, len(finalList)):
+            n.next = ListNode(finalList[i])
+            n = n.next
+        
+        return startNode
+
+
+    # Recursive Solution 
+    finalList = []    
     def reverseRecursively(self, head):
         # Implement this.
+        if head != None:
+            self.finalList.append(head.val)
+            if head.next != None:
+                self.reverseRecursively(head.next)
+        else:
+            self.finalList.reverse()
+            startNode = n = ListNode(self.finalList[0])
+            for i in range(1, len(self.finalList)):
+                n.next = ListNode(self.finalList[i])
+                n = n.next
+            
+            return startNode
+
+
 
 # Test Program
 # Initialize the test list: 
@@ -37,17 +66,16 @@ node2.next = node3
 testTail = ListNode(0)
 node3.next = testTail
 
-print("Initial list: ")
-testHead.printList()
+# print("Initial list: ")
+# testHead.printList()
 # 4 3 2 1 0
 
-testHead.reverseIteratively(testHead)
-#testHead.reverseRecursively(testHead)
 print("List after Iterative reversal: ")
-testTail.printList()
+testHead.reverseIteratively(testHead).printList()
+# testTail.printList()
 # 0 1 2 3 4
 
-testHead.reverseRecursively(testHead)
-print("List after Recursive reversal: ")
-testTail.printList()
+# print("List after Recursive reversal: ")
+# testHead.reverseRecursively(testHead).printList()
+# testTail.printList()
 # 0 1 2 3 4
