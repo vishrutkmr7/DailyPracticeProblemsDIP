a56353fc3cc20724763792faf5eb0d1860affe47
Vishrut <vishrutkmr7@gmail.com>
2019-08-15 08:18:57 +0530

    Solution: 08/14/2019

---
+++ dp08142019.py
@@ -0,0 +1,10 @@
+# This problem was recently asked by Google:
+
+# Given a mathematical expression with just single digits, plus signs, negative signs, and brackets, evaluate the expression.
+# Assume the expression is properly formed.
+
+# def eval(expression):
+    # Fill this in.
+
+print (eval('- (3 + ( 2 - 1 ) )'))
+# -4
