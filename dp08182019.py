4f8ca14a5c0a619c07918f702f6d4ccfce7159e0
Vishrut <vishrutkmr7@gmail.com>
2019-08-18 21:18:46 +0530

    Solution: 08/18/2019, Amazon: Minimal Subarray problem

---
+++ dp08182019.py
@@ -0,0 +1,29 @@
+# This problem was recently asked by Amazon:
+
+# Given an array of n positive integers and a positive integer s,
+# find the minimal length of a contiguous subarray of which the sum ≥ s.
+# If there isn't one, return 0 instead.
+
+class Solution:
+    def minSubArrayLen(self, nums, s):
+        # Fill this in
+        n = len(nums)
+        res = n + 1 # Initialize
+
+        for start in range(0, n):
+            curr_sum = nums[start]
+            if curr_sum > s:
+                return 1
+            
+            for end in range(start + 1, n):
+                curr_sum += nums[end]
+                if curr_sum > s and (end - start) < res:
+                    res = end - start
+        
+        if res == n + 1:
+            return 0
+
+        return res
+
+print (Solution().minSubArrayLen([2, 3, 1, 2, 4, 3], 7))
+# 2
