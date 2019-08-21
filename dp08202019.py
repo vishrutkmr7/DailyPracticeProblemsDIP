528730ee200bcacac16f039bb2e8c50dc8c77165
Vishrut <vishrutkmr7@gmail.com>
2019-08-21 07:43:53 +0530

    Solution: 08/20/2019.py, Apple: Intersection of linked list

--- dp08202019.py
+++ dp08202019.py
@@ -5,15 +5,42 @@
 
 def intersection(a, b):
     # fill this in.
+    curA, curB = a, b
+    lenA, lenB = 0, 0
+    # Finding respective lengths
+    while curA is not None:
+        lenA += 1
+        curA = curA.next
+
+    while curB is not None:
+        lenB += 1
+        curB = curB.next
+
+    curA, curB = a, b
+
+    if lenA > lenB:
+        for i in range(lenA - lenB):
+            curA = curA.next
+    elif lenB > lenA:
+        for i in range(lenB - lenA):
+            curB = curB.next
+    
+    while curB != curA:
+            curB = curB.next
+            curA = curA.next
+    
+    return curA
 
 class Node(object):
     def __init__(self, val):
         self.val = val
         self.next = None
+
     def prettyPrint(self):
         c = self
         while c:
-            print (c.val)
+            print (c.val, end = ' ')
+            # print(c.val),
             c = c.next
 
 a = Node(1)
@@ -23,6 +50,9 @@ a.next.next.next = Node(4)
 
 b = Node(6)
 b.next = a.next.next
+
+# A = 1 -> 2 -> 3 -> 4
+# B = 6 -> 3 -> 4 
 
 c = intersection(a, b)
 c.prettyPrint()
