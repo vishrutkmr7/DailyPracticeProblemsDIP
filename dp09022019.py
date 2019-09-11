4c0b36075a74ac43b11936f7ca17aff279267ab9
Vishrut Jha <vishrutkmr7@gmail.com>
2019-09-11 17:26:54 +0530

    Solution: 09/02/2019, Twitter:Contiguous Subarray with Maximum Sum

--- dp09022019.py
+++ dp09022019.py
@@ -2,8 +2,20 @@
 
 # You are given an array of integers. Find the maximum sum of all possible contiguous subarrays of the array.
 
+
 def max_subarray_sum(arr):
     # Fill this in.
+    n = len(arr)
+    local_max = 0
+    global_max = 0
+    # Kadane's Algorithm
+    for i in range(0, n):
+        local_max = max(arr[i], arr[i] + local_max)
+        if local_max > global_max:
+            global_max = local_max
 
-print (max_subarray_sum([34, -50, 42, 14, -5, 86]))
+    return global_max
+
+
+print(max_subarray_sum([34, -50, 42, 14, -5, 86]))
 # 137
