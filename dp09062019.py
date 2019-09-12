30927c000cf5ee7b0899a9771c886dee5f873866
Vishrut Jha <vishrutkmr7@gmail.com>
2019-09-12 23:35:13 +0530

    Solution: 09/06/2019, AirBNB: Buddy strings

---
+++ dp09062019.py
@@ -0,0 +1,36 @@
+# This problem was recently asked by AirBNB:
+
+# Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
+
+
+class Solution:
+    def buddyStrings(self, A, B):
+        # Fill this in.
+        if len(A) != len(B):
+            return False
+
+        prev = -1
+        curr = -1
+        count = 0
+        i = 0
+
+        while i < len(A):
+            if A[i] != B[i]:
+                count += 1
+                if count > 2:
+                    return False
+                prev = curr
+                curr = i
+            i += 1
+
+        # Check if previous unmatched of A
+        # is equal to curr unmatched of B
+        # and also check for curr unmatched character,
+        # if both are same, then return true
+        return count == 2 and A[prev] == B[curr] and A[curr] == B[prev]
+
+
+print(Solution().buddyStrings("aaaaaaabc", "aaaaaaacb"))
+# True
+print(Solution().buddyStrings("aaaaaabbc", "aaaaaaacb"))
+# False
