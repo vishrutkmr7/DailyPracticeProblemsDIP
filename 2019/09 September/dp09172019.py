3a7e654ac72b58f8ce47ba59b3d2059395113580
Vishrut Jha <vishrutkmr7@gmail.com>
2019-09-18 12:31:19 +0530

    Solution: 09/17/2019, Twitter: 3 Sum

---
+++ 2019/09 September/dp09172019.py
@@ -0,0 +1,41 @@
+# This problem was recently asked by Twitter:
+
+# Given an array, nums, of n integers, find all unique triplets (three numbers, a, b, & c) in nums such that
+# a + b + c = 0. Note that there may not be any triplets that sum to zero in nums, and that the triplets must not be duplicates.
+
+
+class Solution(object):
+    def threeSum(self, nums):
+        # Fill this in.
+        n = len(nums)
+        found = False
+        nums.sort()
+        ans = []
+
+        for i in range(0, n - 1):
+            l = i + 1
+            r = n - 1
+            x = nums[i]
+            while l < r:
+                if (x + nums[l] + nums[r]) == 0:
+                    p = [x, nums[l], nums[r]]
+                    if p not in ans:
+                        ans.append(p)
+                    l += 1
+                    r -= 1
+                    found = True
+                elif (x + nums[l] + nums[r]) < 0:
+                    l += 1
+                else:
+                    r -= 1
+
+        if not found:
+            return "No Triplets found"
+        else:
+            return ans
+
+
+# Test Program
+nums = [1, -2, 1, 0, 5]
+print(Solution().threeSum(nums))
+# [[-2, 1, 1]]
