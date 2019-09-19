7e7f9a6ca8185fe34791519da3ff00bef3c76476
Vishrut Jha <vishrutkmr7@gmail.com>
2019-09-19 16:09:13 +0530

    Solution: 09182019, Twitter: Largest BST in a Binary Tree

---
+++ 2019/09 September/dp09182019.py
@@ -0,0 +1,77 @@
+# This problem was recently asked by Twitter:
+
+# You are given the root of a binary tree. Find and return the largest subtree of that tree, which is a valid binary search tree.
+
+
+class TreeNode:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.key = key
+
+    def __str__(self):
+        # preorder traversal
+        answer = str(self.key)
+        if self.left:
+            answer += str(self.left)
+        if self.right:
+            answer += str(self.right)
+        return answer
+
+
+res = []
+
+
+def largestBST(root):
+    largest_bst_subtree(root)
+    return getValue(res)
+
+
+def largest_bst_subtree(root):
+    # Fill this in.
+    if (root is None) or (root.left is None and root.right is None):
+        return root
+
+    l = largest_bst_subtree(root.left)
+    r = largest_bst_subtree(root.right)
+
+    if isBST(root):
+        p = {"tree": root, "size": size(root)}
+        res.append(p)
+
+
+def isBST(root, l=None, r=None):
+    if root == None:
+        return True
+    if l != None and root.key <= l.key:
+        return False
+    if r != None and root.key >= r.key:
+        return False
+    return isBST(root.left, l, root) and isBST(root.right, root, r)
+
+
+def size(node):
+    if node is None:
+        return 0
+    else:
+        return size(node.left) + 1 + size(node.right)
+
+
+def getValue(result):
+    ans = sorted(result, key=lambda i: i["size"], reverse=True)
+    return ans[0]["tree"]
+
+
+#     5
+#    / \
+#   6   7
+#  /   / \
+# 2   4   9
+node = TreeNode(5)
+node.left = TreeNode(6)
+node.right = TreeNode(7)
+node.left.left = TreeNode(2)
+node.right.left = TreeNode(4)
+node.right.right = TreeNode(9)
+print(largestBST(node))
+# 749
