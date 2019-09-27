595c72be0a1e9b0e5800f9546a019bd9bd47f1e6
Vishrut Jha <vishrutkmr7@gmail.com>
2019-09-27 11:41:22 +0530

    Solution: 09/26/2019, Amazon: Product of Array Except Self

---
+++ 2019/09 September/dp09262019.py
@@ -0,0 +1,20 @@
+# This problem was recently asked by Amazon:
+
+# You are given an array of integers. Return an array of the same size where the element at each index
+# is the product of all the elements in the original array except for the element at that index.
+
+
+def products(nums):
+    # Fill this in.
+    prod = 1
+    for i in nums:
+        prod *= i
+
+    for i in range(0, len(nums)):
+        nums[i] = int(prod / nums[i])
+
+    return nums
+
+
+print(products([1, 2, 3, 4, 5]))
+# [120, 60, 40, 30, 24]
