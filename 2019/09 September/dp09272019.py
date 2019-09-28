a6ffbce1b86c58767ebcd8774cc86b4945583f2f
Vishrut Jha <vishrutkmr7@gmail.com>
2019-09-28 13:39:03 +0530

    Solution: 09/27/2019, Amazon: Intersection of 2 arrays

---
+++ 2019/09 September/dp09272019.py
@@ -0,0 +1,19 @@
+# This problem was recently asked by Amazon:
+
+# Given two arrays, write a function to compute their intersection - the intersection means the numbers that are in both arrays.
+
+
+class Solution:
+    def intersection(self, nums1, nums2):
+        # Fill this in.
+        res = []
+        for i in nums1:
+            if i in nums2:
+                res.append(i)
+        
+        res = list(set(res))
+        return res
+
+
+print (Solution().intersection([4, 9, 5], [9, 4, 9, 8, 4]))
+# [9, 4]
