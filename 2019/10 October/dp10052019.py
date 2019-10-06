cf90da6d9de2e7e0d59cd527b473dd29cfc92fba
Vishrut Jha <vishrutkmr7@gmail.com>
2019-10-06 12:15:37 +0530

    Solution: 10/05/2019,  Uber: MinStack

---
+++ 2019/10 October/dp10052019.py
@@ -0,0 +1,49 @@
+# This problem was recently asked by Uber:
+
+# Design a simple stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+# push(x) -- Push element x onto stack.
+# pop() -- Removes the element on top of the stack.
+# top() -- Get the top element.
+# getMin() -- Retrieve the minimum element in the stack.
+
+# Note: be sure that pop() and top() handle being called on an empty stack.
+
+
+class minStack(object):
+    def __init__(self):
+        # Fill this in.
+        self.items = []
+
+    def push(self, x):
+        # Fill this in.
+        self.items.append(x)
+
+    def pop(self):
+        # Fill this in.
+        if len(self.items) == 0:
+            return None
+        return self.items.pop()
+
+    def top(self):
+        # Fill this in.
+        return self.items[len(self.items) - 1]
+
+    def getMin(self):
+        # Fill this in.
+        if len(self.items) == 0:
+            return None
+        return min(self.items)
+
+
+x = minStack()
+x.push(-2)
+x.push(0)
+x.push(-3)
+print(x.getMin())
+# -3
+x.pop()
+print(x.top())
+# 0
+print(x.getMin())
+# -2
