5f790dbadc6a291ff1deaf9f1930c5e1a04413dd
Vishrut Jha <vishrutkmr7@gmail.com>
2019-10-12 23:29:08 +0530

    Solution: 10/12/2019, Microsoft: Max and Min with Limited Comparisons

--- 2019/10 October/dp10122019.py
+++ 2019/10 October/dp10122019.py
@@ -7,7 +7,29 @@
 def find_min_max(nums):
     # Fill this in.
     n = len(nums)
+    if n % 2 == 0:
+        mx = max(nums[0], nums[1])
+        mn = min(nums[0], nums[1])
+        i = 2
+    else:
+        mx = mn = nums[0]
+        i = 1
+    while i < n - 1:
+        if nums[i] < nums[i + 1]:
+            mx = max(mx, nums[i + 1])
+            mn = min(mn, nums[i])
+        else:
+            mx = max(mx, nums[i])
+            mn = min(mn, nums[i + 1])
+        i += 2
 
+    return mn, mx
+
+
+# If n is odd then initialize min and max as first element.
+# If n is even then initialize min and max as minimum and maximum of the first two elements respectively.
+# For rest of the elements, pick them in pairs and compare their
+# maximum and minimum with max and min respectively.
 
 print(find_min_max([3, 5, 1, 2, 4, 8]))
 # (1, 8)
