17cfa6e5c41452ee733c9b1e8e7a6fd20799d0cf
Vishrut Jha <vishrutkmr7@gmail.com>
2019-11-01 15:52:41 +0530

    Solution: 10/31/2019, Uber: Detect Linked List Cycle

---
+++ 2019/10 October/dp10312019.py
@@ -0,0 +1,32 @@
+#  This problem was recently asked by Uber:
+
+# Given a linked list, determine if the linked list has a cycle in it.
+# For notation purposes, we use an integer pos which represents the zero-indexed position where the tail connects to.
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        # Fill this in.
+        if head is None:
+            return
+
+
+testHead = ListNode(4)
+node1 = ListNode(3)
+testHead.next = node1
+node2 = ListNode(2)
+node1.next = node2
+node3 = ListNode(1)
+node2.next = node3
+testTail = ListNode(0)
+node3.next = testTail
+testTail.next = node1
+
+print(Solution().hasCycle(testHead))
+# True
