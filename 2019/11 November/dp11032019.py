ef355dd2ff48af7a8dedf43062e6c4aab28a582a
Vishrut Jha <vishrutkmr7@gmail.com>
2019-11-04 10:45:20 +0530

    Solution: 11/03/2019, Facebook: Absolute Path

---
+++ 2019/11 November/dp11032019.py
@@ -0,0 +1,27 @@
+# This problem was recently asked by Facebook:
+
+# Given a file path with folder names, '..' (Parent directory), and '.' (Current directory),
+# return the shortest possible file path (Eliminate all the '..' and '.').
+
+
+def shortest_path(file_path):
+    # Fill this in.
+    inPath = file_path.split("/")
+    resStack = []
+    for path in inPath:
+        if path == "..":
+            del resStack[-1]
+        elif path == ".":
+            continue
+        else:
+            resStack.append(path)
+    resPath = ""
+    del resStack[0]
+    for path in resStack:
+        resPath = resPath + "/" + path
+
+    return resPath
+
+
+print(shortest_path("/Users/Joma/Documents/../Desktop/./../"))
+# /Users/Joma/
