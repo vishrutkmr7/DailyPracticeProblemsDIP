c7b539098308a5427a9c9f4325857774f236fdc4
Vishrut Jha <vishrutkmr7@gmail.com>
2019-11-10 18:04:49 +0530

    spreadsheet: added inverse due to repeated problem (11/06/2019)

--- 2019/11 November/dp11062019.py
+++ 2019/11 November/dp11062019.py
@@ -4,6 +4,8 @@
 # In other words, column 1 is named "A", column 2 is "B", column 26 is "Z", column 27 is "AA" and so forth.
 # Given a positive integer, find its corresponding column name.
 
+import string
+
 
 class Solution:
     def convertToTitle(self, n):
@@ -13,6 +15,13 @@ class Solution:
             n, remainder = divmod(n - 1, 26)
             ans = chr(65 + remainder) + ans
         return ans
+
+    def convertToNum(self, col):
+        num = 0
+        for c in col:
+            if c in string.ascii_letters:
+                num = num * 26 + (ord(c.upper()) - ord("A")) + 1
+        return num
 
 
 input1 = 1
@@ -24,3 +33,12 @@ print(Solution().convertToTitle(input2))
 # YYYZ
 print(Solution().convertToTitle(input3))
 # AB
+input4 = "Z"
+input5 = "AA"
+input6 = "ABY"
+print(Solution().convertToNum(input4))
+# 26
+print(Solution().convertToNum(input5))
+# 27
+print(Solution().convertToNum(input6))
+# 753
