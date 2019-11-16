83be33bdfd42666600ac49123a52b3908c088647
Vishrut Jha <vishrutkmr7@gmail.com>
2019-11-16 20:31:30 +0530

    Incomplete sol

--- 2019/11 November/dp11152019.py
+++ 2019/11 November/dp11152019.py
@@ -1,7 +1,6 @@
 # This problem was recently asked by AirBNB:
 
 # We have a list of tasks to perform, with a cooldown period. We can do multiple of these at the same time, but we cannot run the same task simultaneously.
-
 # Given a list of tasks, find how long it will take to complete the tasks in the order they are input.
 
 
@@ -9,6 +8,13 @@ def findTime(arr, cooldown):
     # Fill this in.
     resArr = []
     n = len(arr)
+    for i in range(0, n):
+        resArr.append(arr[i])
+        # if arr[i] == arr[i + 1]:
+        #     for j in range(0, cooldown):
+        #         resArr.append("_")
+
+    return len(resArr)
 
 
 print(findTime([1, 1, 2, 1], 2))
