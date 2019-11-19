d5ff30c5c378ecc6be6ea383b9d79c9e7bd785c3
Vishrut Jha <vishrutkmr7@gmail.com>
2019-11-19 10:41:57 +0530

    Solution: 11/18/2019, Facebook: Kaprekar's Constant

---
+++ 2019/11 November/dp11182019.py
@@ -0,0 +1,45 @@
+# This problem was recently asked by Facebook:
+
+# Kaprekar's Constant is the number 6174. This number is special because it has the property where for any 4-digit number
+# that has 2 or more unique digits, if you repeatedly apply a certain function it always reaches the number 6174.
+# This certain function is as follows:
+# - Order the number in ascending form and descending form to create 2 numbers.
+# - Pad the descending number with zeros until it is 4 digits in length.
+# - Subtract the ascending number from the descending number.
+# - Repeat.
+# Given a number n, find the number of times the function needs to be applied to reach Kaprekar's constant
+
+
+KAPREKAR_CONSTANT = 6174
+
+
+def kaprekar(n):
+    ascending = "".join(sorted(str(n)))
+    descending = "".join(sorted(str(n), reverse=True))
+    # Padding
+    if len(descending) != 4:
+        p = 4 - len(descending)
+        ad = ""
+        for i in range(p):
+            ad += "0"
+        descending += ad
+    return int(descending) - int(ascending)
+
+
+def num_kaprekar_iterations(n):
+    # Fill this in.
+    count = 1
+    kap = kaprekar(n)
+    while kap != KAPREKAR_CONSTANT:
+        kap = kaprekar(kap)
+        count += 1
+
+    return count
+
+
+print(num_kaprekar_iterations(123))
+# 3
+# Explanation:
+#  3210 - 123 = 3087
+#  8730 - 0378 = 8352
+#  8532 - 2358 = 6174 (3 iterations)
