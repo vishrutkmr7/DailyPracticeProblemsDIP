9e60501ebdffef051794ad8e94912840a716c4d5
Vishrut Jha <vishrutkmr7@gmail.com>
2019-11-30 11:07:27 +0530

    Solution: 11/29/2019, AirBNB: Rotate Linked List

---
+++ 2019/11 November/dp11292019.py
@@ -0,0 +1,50 @@
+#  This problem was recently asked by AirBNB:
+
+# Given a linked list and a number k, rotate the linked list by k places.
+
+
+class Node:
+    def __init__(list, value, next=None):
+        list.value = value
+        list.next = next
+
+    def __str__(list):
+        current = list
+        ret = ""
+        while current:
+            ret += str(current.value)
+            current = current.next
+        return ret
+
+
+def rotate_list(list, k):
+    # Fill this in.
+    if k == 0:
+        return
+
+    current = list
+    count = 1
+    while count < k and current is not None:
+        current = current.next
+        count += 1
+
+    if current is None:
+        return
+
+    kthNode = current
+    while current.next is not None:
+        current = current.next
+
+    current.next = list
+    list = kthNode.next
+    kthNode.next = None
+
+    return list
+
+
+# Order is 1, 2, 3, 4
+llist = Node(1, Node(2, Node(3, Node(4))))
+
+# Order should now be 3, 4, 1, 2
+print(rotate_list(llist, 2))
+# 3412
