c64a0fd54c0c1e026915a1bf37af0d51b6bc1c48
Vishrut Jha <vishrutkmr7@gmail.com>
2019-12-18 13:18:39 +0530

    Solution: 12/17/2019, Facebook: Rotate Array

---
+++ 2019/12 December/dp12172019.py
@@ -0,0 +1,17 @@
+#  This problem was recently asked by Facebook:
+
+# Given an array and an integer k, rotate the array by k spaces. Do this without generating a new array and without using extra space.
+
+
+def rotate_list(nums, k):
+    # Fill this in.
+    n = len(nums)
+    for i in range(0, k):
+        a.append(a[0])
+        a.pop(0)
+
+
+a = [1, 2, 3, 4, 5]
+rotate_list(a, 2)
+print(a)
+# [3, 4, 5, 1, 2]
