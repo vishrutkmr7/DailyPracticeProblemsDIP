5d79dd5e86fd3f4ac8faca4aee4e902ba5e8d7ca
Vishrut Jha <vishrutkmr7@gmail.com>
2019-12-21 10:27:16 +0530

    Solution: 12/20/2019, Microsoft: Sorted Square Numbers

---
+++ 2019/12 December/dp12202019.py
@@ -0,0 +1,16 @@
+# This problem was recently asked by Microsoft:
+
+# Given a list of sorted numbers (can be both negative or positive), return the list of numbers squared in sorted order.
+
+
+def square_numbers(nums):
+    # Fill this in.
+    res = []
+    for i in nums:
+        res.append(i * i)
+
+    res.sort()
+    return res
+
+print(square_numbers([-5, -3, -1, 0, 1, 4, 5]))
+# [0, 1, 1, 9, 16, 25, 25]
