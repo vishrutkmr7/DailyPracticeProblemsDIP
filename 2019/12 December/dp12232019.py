67b91da7d6d1239450888ec197c8817056959a08
Vishrut Jha <vishrutkmr7@gmail.com>
2019-12-24 11:03:03 +0530

    Solution: 12/23/2019, Apple: Reverse words

---
+++ 2019/12 December/dp12232019.py
@@ -0,0 +1,31 @@
+#  This problem was recently asked by Apple:
+
+# Given a list of words in a string, reverse the words in-place (ie don't create a new string and reverse the words).
+# Since python strings are not mutable, you can assume the input will be a mutable sequence (like list).
+
+
+def reverse_words(words):
+    # Fill this in.
+    wordArr = []
+    temp = ""
+
+    for char in words:
+        if char is not " ":
+            temp += char
+        else:
+            wordArr.append(temp)
+            temp = ""
+
+    wordArr.append(temp)  # last word
+    wordArr.reverse()
+    temp = ""
+
+    for word in wordArr:
+        temp += word + " "
+
+    return temp
+
+
+s = list("can you read this")
+print(reverse_words(s))
+# this read you can
