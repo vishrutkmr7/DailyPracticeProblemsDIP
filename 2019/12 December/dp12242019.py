db929b3f6ac8999ade3e6c0a64f9368c6ff3dc68
Vishrut Jha <vishrutkmr7@gmail.com>
2019-12-25 11:09:44 +0530

    Solution: 12/24/2019, Twitter: Anagrams in a string

---
+++ 2019/12 December/dp12242019.py
@@ -0,0 +1,41 @@
+# This problem was recently asked by Twitter:
+
+# Given 2 strings s and t, find and return all indexes in string s where t is an anagram.
+
+
+MAX = 256
+
+
+def compare(arr1, arr2):
+    for i in range(MAX):
+        if arr1[i] != arr2[i]:
+            return False
+    return True
+
+
+def find_anagrams(s, t):
+    # Fill this in.
+    M = len(t)
+    N = len(s)
+    res = []
+    countP = [0] * MAX
+    countTW = [0] * MAX
+
+    for i in range(M):
+        (countP[ord(t[i])]) += 1
+        (countTW[ord(s[i])]) += 1
+
+    for i in range(M, N):
+        if compare(countP, countTW):
+            res.append(i - M)
+        (countTW[ord(s[i])]) += 1
+        (countTW[ord(s[i - M])]) -= 1
+
+    if compare(countP, countTW):
+        res.append(N - M)
+
+    return res
+
+
+print(find_anagrams("acdbacdacb", "abc"))
+# [3, 7]
