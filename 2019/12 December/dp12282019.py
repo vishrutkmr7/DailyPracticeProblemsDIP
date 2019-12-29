d6b935dd38a07b706903864f893f5d6087db9913
Vishrut Jha <vishrutkmr7@gmail.com>
2019-12-29 10:28:55 +0530

    Solution: 12/28/2019, AirBNB: Power Function

---
+++ 2019/12 December/dp12282019.py
@@ -0,0 +1,20 @@
+# em was recently asked by AirBNB:
+
+# The power function calculates x raised to the nth power.
+# If implemented in O(n) it would simply be a for loop over n and multiply x n times.
+# Instead implement this power function in O(log n) time. You can assume that n will be a non-negative integer.
+
+
+def pow(x, n):
+  # Fill this in.
+    if(n == 0): return 1
+    temp = pow(x, int(n / 2))  
+      
+    if (n % 2 == 0): 
+        return temp * temp 
+    else: 
+        if(n > 0): return x * temp * temp 
+        else: return (temp * temp) / x 
+
+print(pow(5, 3))
+# 125
