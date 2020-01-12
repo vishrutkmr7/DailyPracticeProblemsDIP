cbbff8abd420a61908c45e9e9db81b8c06bb45ec
Vishrut Jha <vishrutkmr7@gmail.com>
2020-01-12 14:33:48 +0530

    Solution: 01/11/2020, Uber: Duplicate Subtrees

---
+++ 2020/01 January/dp01112020.py
@@ -0,0 +1,63 @@
+# This problem was recently asked by Uber:
+
+# Given a binary tree, find all duplicate subtrees (subtrees with the same value and same structure)
+# and return them as a list of list [subtree1, subtree2, ...] where subtree1 is a duplicate of subtree2 etc.
+import collections
+
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        if self.left and self.right:
+            return f"({self.value}, {self.left}, {self.right})"
+        if self.left:
+            return f"({self.value}, {self.left})"
+        if self.right:
+            return f"({self.value}, None, {self.right})"
+        return f"({self.value})"
+
+
+def dup_trees(root):
+    # Fill this in.
+    count = collections.Counter()
+    ans = []
+
+    def collect(node):
+        if not node:
+            return "#"
+        serial = "{},{},{}".format(node.value, collect(node.left), collect(node.right))
+        count[serial] += 1
+        if count[serial] == 2:
+            ans.append(node)
+        return serial
+
+    collect(root)
+    return ans
+
+
+n3_1 = Node(3)
+n2_1 = Node(2, n3_1)
+n3_2 = Node(3)
+n2_2 = Node(2, n3_2)
+n1 = Node(1, n2_1, n2_2)
+# Looks like
+#     1
+#    / \
+#   2   2
+#  /   /
+# 3   3
+
+print(dup_trees(n1))
+# [[(3), (3)], [(2, (3)), (2, (3))]]
+# There are two duplicate trees
+#
+#     2
+#    /
+#   3
+# and just the leaf
+#
+# 3
