30b9713407f73a547ef5e98c57f3ba8766cba979
Vishrut Jha <vishrutkmr7@gmail.com>
2020-01-21 10:32:27 +0530

    SOlution: 01/20/2020, Apple: Remove Adjacent Duplicate Characters

---
+++ 2020/01 January/dp01202020.py
@@ -0,0 +1,51 @@
+# This problem was recently asked by Apple:
+
+# Given a string, we want to remove 2 adjacent characters that are the same, and repeat the process with the new string until we can no longer perform the operation.
+
+
+def removeUtil(string, last_removed):
+    if len(string) == 0 or len(string) == 1:
+        return string
+
+    if string[0] == string[1]:
+        last_removed = ord(string[0])
+        while len(string) > 1 and string[0] == string[1]:
+            string = string[1:]
+        string = string[1:]
+
+        return removeUtil(string, last_removed)
+
+    rem_str = removeUtil(string[1:], last_removed)
+
+    if len(rem_str) != 0 and rem_str[0] == string[0]:
+        last_removed = ord(string[0])
+        return rem_str[1:]
+
+    if len(rem_str) == 0 and last_removed == ord(string[0]):
+        return rem_str
+
+    return [string[0]] + rem_str
+
+
+def remove_adjacent_dup(s):
+    # Fill this in.
+    last_removed = 0
+    return toString(removeUtil(toList(s), last_removed))
+
+
+def toList(string):
+    x = []
+    for i in string:
+        x.append(i)
+    return x
+
+
+def toString(x):
+    return "".join(x)
+
+
+print(remove_adjacent_dup("cabba"))
+# Start with cabba
+# After remove bb: caa
+# After remove aa: c
+# print c
