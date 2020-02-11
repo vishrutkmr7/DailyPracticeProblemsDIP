03a975a8454defe09e67e1bd2e7e5ee9a1946af1
Vishrut Jha <vishrutkmr7@gmail.com>
2020-02-11 10:53:39 +0530

    Solution: 02/10/2020, Google: Number of 1 bits

---
+++ 2020/02 February/dp02102020.py
@@ -0,0 +1,23 @@
+# This problem was recently asked by Google:
+
+# Given an integer, find the number of 1 bits it has.
+
+
+def getSum(n):
+    sum = 0
+    while n > 0:
+        sum += int(n % 10)
+        n = int(n / 10)
+    return sum
+
+
+def one_bits(num):
+    # Fill this in.
+    bits = str(bin(num))
+    n = int(bits[2:])  # remove 0b
+    return getSum(n)
+
+
+print(one_bits(23))
+# 4
+# 23 = 0b1011
