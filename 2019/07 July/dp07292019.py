6ee76e3e2d3441cb3b43c09a0801f0f828e834a2
Vishrut Jha <vishrutkmr7@gmail.com>
2020-02-15 19:36:23 +0530

    spatial change

--- 2019/07 July/dp07292019.py
+++ 2019/07 July/dp07292019.py
@@ -7,34 +7,36 @@ class ListNode(object):
         self.val = x
         self.next = None
 
+
 class Solution:
-  def addTwoNumbers(self, l1, l2, c = 0):
-    # Fill this in.
-    p1 = l1
-    p2 = l2
-    i = 0
-    j = 0
-    n1 = 0
-    n2 = 0
-    while p1:
-        n1 += p1.val * (10 ** i)
-        i +=1
-        p1 = p1.next
+    def addTwoNumbers(self, l1, l2, c=0):
+        # Fill this in.
+        p1 = l1
+        p2 = l2
+        i = 0
+        j = 0
+        n1 = 0
+        n2 = 0
+        while p1:
+            n1 += p1.val * (10 ** i)
+            i += 1
+            p1 = p1.next
 
-    while p2:
-        n2 += p2.val * (10 ** j)
-        j +=1
-        p2 = p2.next
+        while p2:
+            n2 += p2.val * (10 ** j)
+            j += 1
+            p2 = p2.next
 
-    s = n1 + n2
+        s = n1 + n2
 
-    start_node = node = ListNode(str(s)[-1])  # initialize to last character in s
-    for c in reversed(str(s)[:-1]):
-        node.next = ListNode(c)
-        node = node.next
-    
-    # print("n1", n1, "n2", n2)
-    return start_node
+        start_node = node = ListNode(str(s)[-1])  # initialize to last character in s
+        for c in reversed(str(s)[:-1]):
+            node.next = ListNode(c)
+            node = node.next
+
+        # print("n1", n1, "n2", n2)
+        return start_node
+
 
 l1 = ListNode(2)
 l1.next = ListNode(4)
@@ -46,6 +48,6 @@ l2.next.next = ListNode(4)
 
 result = Solution().addTwoNumbers(l1, l2)
 while result:
-  print (result.val),
-  result = result.next
+    print(result.val),
+    result = result.next
 # 7 0 8
