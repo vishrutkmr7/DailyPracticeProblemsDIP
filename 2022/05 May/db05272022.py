e9a0aed107133632f3507f3aed1784cbdd5b30e6
Vishrut Jha <vishrutkmr7@gmail.com>
2022-05-27 14:47:03 +0000

    Beginning of a new series: DailyByte using GitHub CodeSpaces

---
+++ 2022/05 May/db05272022.py
@@ -0,0 +1,21 @@
+"""
+#This question is asked by Google.
+#Given a string, reverse all of its characters and return the resulting string.
+
+#Ex: Given the following strings...
+#“Cat”, return “taC”
+#“The Daily Byte”, return "etyB yliaD ehT”
+#“civic”, return “civic”
+
+"""
+
+
+def reverse_string(string):
+    """A dummy docstring."""
+    return string[::-1]
+
+
+# Test cases
+print(reverse_string("Cat"))
+print(reverse_string("The Daily Byte"))
+print(reverse_string("civic"))
