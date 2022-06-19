7836b5a2c6201a13e540c04df8a659a508aad763
Vishrut Jha <vishrutkmr7@gmail.com>
2022-06-19 21:08:18 +0000

    Solution: 06/19/2022, Facebook: Remove adjacent duplicates

---
+++ 2022/06 June/db06192022.py
@@ -0,0 +1,27 @@
+"""
+This question is asked by Facebook.
+Given a string s containing only lowercase letters, continuously remove adjacent characters that are the same and return the result. 
+
+Ex: Given the following strings...
+
+s = "abccba", return ""
+s = "foobar", return "fbar"
+s = "abccbefggfe", return "a"
+"""
+
+
+def remove_adjacent_duplicates(string):
+    """Using stack for this problem."""
+    stack = []
+    for char in string:
+        if stack and stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+    return "".join(stack)
+
+
+# Test Cases
+print(remove_adjacent_duplicates("abccba"))
+print(remove_adjacent_duplicates("foobar"))
+print(remove_adjacent_duplicates("abccbefggfe"))
