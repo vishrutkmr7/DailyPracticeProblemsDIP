fec2704c4197825bb731a54580cbe2d085474762
Vishrut Jha <vishrutkmr7@gmail.com>
2022-07-11 15:24:19 -0700

    Create numberOfInversions.py

---
+++ 2022/07 July/numberOfInversions.py
@@ -0,0 +1,55 @@
+class Solution:
+	  # @param A : list of integers
+	  # @return an integer
+    def merge(self, arr, left, mid, right):
+        i = left
+        j = mid
+        k = 0
+        invCount = 0
+        temp = [0 for x in range(right - left + 1)]
+    
+        while (i < mid) and (j <= right):
+            if arr[i] <= arr[j]:
+                temp[k] = arr[i]
+                k += 1
+                i += 1
+    
+            else:
+                temp[k] = arr[j]
+                invCount += mid - i
+                k += 1
+                j += 1
+    
+        while i < mid:
+            temp[k] = arr[i]
+            k += 1
+            i += 1
+    
+        while j <= right:
+            temp[k] = arr[j]
+            k += 1
+            j += 1
+    
+        k = 0
+        for i in range(left, right + 1):
+            arr[i] = temp[k]
+            k += 1
+    
+        return invCount
+    
+        
+    def mergeSort(self, arr, left, right):
+        invCount = 0
+    
+        if right > left:
+            mid = (right + left) // 2
+    
+            invCount = self.mergeSort(arr, left, mid)
+            invCount += self.mergeSort(arr, mid + 1, right)
+            invCount += self.merge(arr, left, mid + 1, right)
+    
+        return invCount
+    
+    
+    def countInversions(self, arr):
+        return self.mergeSort(arr, 0, len(arr) - 1)
