dd217be48cc6e0eb3ecb339e24f968d163f0da7e
Vishrut Jha <vishrutkmr7@gmail.com>
2022-07-13 21:31:01 +0000

    Solution: 07/12/2022, Symmetric BST

---
+++ 2022/07 July/db07122022.py
@@ -0,0 +1,53 @@
+"""
+Given a binary tree, return whether or not it forms a reflection across its center
+(i.e. a line drawn straight down starting from the root). 
+Note: a reflection is when an image, flipped across a specified line, forms the same image.
+
+Ex: Given the following tree…
+
+   2
+ /   \
+1     1
+return true as when the tree is reflected across its center all the nodes match.
+Ex: Given the following tree…
+
+    1
+   / \
+  5   5
+   \    \
+    7    7
+return false as when the tree is reflected across its center the nodes containing sevens do not match.
+"""
+
+
+class BinaryTree:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def is_symmetric(tree):
+    return is_symmetric_helper(tree, tree)
+
+
+def is_symmetric_helper(tree1, tree2):
+    if not tree1 and not tree2:
+        return True
+    if not tree1 or not tree2:
+        return False
+    return tree1.val == tree2.val and is_symmetric_helper(tree1.left, tree2.right) and is_symmetric_helper(tree1.right, tree2.left)
+
+
+# Test Cases
+tree = BinaryTree(2)
+tree.left = BinaryTree(1)
+tree.right = BinaryTree(1)
+print(is_symmetric(tree))
+
+tree = BinaryTree(1)
+tree.left = BinaryTree(5)
+tree.right = BinaryTree(5)
+tree.left.right = BinaryTree(7)
+tree.right.right = BinaryTree(7)
+print(is_symmetric(tree))
