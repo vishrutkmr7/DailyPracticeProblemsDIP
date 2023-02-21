ebd50fbfd2254692cca1b723edb6ddabffc06a21
Vishrut Jha <vishrutkmr7@gmail.com>
2023-02-21 21:39:19 +0000

    Solution: 02/21/2023, Find Words That Can Be Formed by Characters

---
+++ 2023/02 February/db02212023.py
@@ -0,0 +1,19 @@
+"""
+Given an array of words and a string of characters, chars, return the sum of the lengths of words that can be formed
+using only the chars.
+Note: Each character within chars can only be used once.
+
+Ex: Given the following words and chars…
+
+words = ["abc", "cab"], chars = "bac", return 6 ("abc"'s length + "cab"'s length).
+"""
+
+
+class Solution:
+    def countCharacters(self, words: list[str], chars: str) -> int:
+        count = 0
+        for word in words:
+            flag = all(word.count(i) <= chars.count(i) for i in word)
+            if flag:
+                count += len(word)
+        return count
