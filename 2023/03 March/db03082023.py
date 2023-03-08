baeb459525189d27f4b0689cdaaf3975b7af46cf
Vishrut Jha <vishrutkmr7@gmail.com>
2023-03-08 19:44:54 +0000

    Solution: 03/08/2023, isMagical

---
+++ 2023/03 March/db03082023.py
@@ -0,0 +1,26 @@
+"""
+Given a k-digit integer, num, return whether or not the number if magical.
+Note: A magical numbers is a number in which the sum of all its digits
+raised to the kth power sum to the number itself.
+
+Ex: Given the following num…
+
+num = 153, return true (1^3 + 5^3 + 3^3 = 153).
+Ex: Given the following num…
+
+num = 38, return false.
+"""
+
+
+class Solution:
+    def isMagical(self, num: int) -> bool:
+        k = len(str(num))
+        return num == sum(int(digit) ** k for digit in str(num))
+
+
+# Test Cases
+if __name__ == "__main__":
+    solution = Solution()
+    assert solution.isMagical(153) is True
+    assert solution.isMagical(38) is False
+    print("All tests passed successfully.")
